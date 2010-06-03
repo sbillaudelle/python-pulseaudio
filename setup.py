abe126a4486c6579f8577f25c9dcc5e5d6c656c4
Sebastian Billaudelle <stein@cream-project.org>
2010-06-03 20:47:54 +0200

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name = 'python-pulseaudio',
+    version = '0.0.5',
+    description = 'Simple bindings to PulseAudio\'s volume control interfaces.',
+    author = 'Sebastian Billaudelle',
+    author_email = 'stein@cream-project.org',
+    url = 'http://github.com/sbillaudelle/python-pulseaudio',
+    license = 'WTFPL',
+    packages = ['pulseaudio']
+)
